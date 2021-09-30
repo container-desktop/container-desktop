bcbc1a80123d682adf1b3bf8e1bea9a36872f7aa
Mark Rexwinkel <markr@infosupport.com>
2021-09-30 13:21:16 +0200

    Seperated docker data into its own distro (#7)

---
+++ deployment/container-desktop-data/Dockerfile
@@ -0,0 +1,3 @@
+FROM busybox
+COPY wsl-init-data.sh .
+RUN mkdir data && chmod +x wsl-init-data.sh
---
+++ deployment/container-desktop-data/wsl-init-data.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+mkdir -p /mnt/wsl/container-desktop-data
+mount --bind / /mnt/wsl/container-desktop-data
